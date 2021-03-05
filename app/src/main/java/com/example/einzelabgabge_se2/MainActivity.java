10dd920e01887942843325de4f3d470256e3b206
tina <kristina.liebhart@gmail.com>
2021-03-05 21:06:50 +0100

    tcp connection - implementation finished

--- app/src/main/java/com/example/einzelabgabge_se2/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabge_se2/MainActivity.java
@@ -2,122 +2,79 @@ package com.example.einzelabgabge_se2;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.KeyEvent;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-    TextView textView2;
-    EditText textInputEditText;
+    //Text Input field
+    private EditText input_number;
+
+    //Text Output field
+    private TextView displayServerCalculation;
+    private TextView displayClientCalculation;
+
+    //Button
+    private Button serverButton;
+    private  Button calcNumButton;
+
+    private String matrikelnummer;
+    private String resultFromServer;
+
+
+    //Create Instances
+    ASCII ascii = new ASCII();
+    TCP tcp = new TCP(matrikelnummer);
+
+
+    Thread thread = new Thread(tcp);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textInputEditText = (EditText)findViewById(R.id.textInputEditText);
-        button=(Button)findViewById(R.id.button);
-        textView2=(TextView)findViewById(R.id.textView2);
+
+        //access to fields via id
+        input_number = (EditText) findViewById(R.id.textInputEditText);
+        displayServerCalculation = (TextView) findViewById(R.id.textView2);
+        displayClientCalculation = (TextView) findViewById(R.id.textView3);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        //add Eventlistener to the Server-Button
+        serverButton = (Button) findViewById(R.id.button);
+        serverButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String number = textInputEditText.getText().toString();
-                String erg = getAsci(number);
-                textView2.setText(erg);
+            public void onClick(View view) {
+                matrikelnummer = input_number.getText().toString();
+                //connect();
+                tcp.setMartriculationNumber(matrikelnummer);
+
+                thread.start();
+
+                try {
+                    thread.join(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                resultFromServer = tcp.getModiefiedNumber();
+                displayServerCalculation.setText(resultFromServer);
+            }
+        });
+
+        calcNumButton = (Button) findViewById(R.id.button2);
+        calcNumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                matrikelnummer = input_number.getText().toString();
+                String resultFromCalculation = ascii.getAscii(matrikelnummer);
+                displayClientCalculation.setText(resultFromCalculation);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        setContentView(R.layout.activity_main);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public String getAsci(String number){
-        String ergebnis= "";
-        char newChar;
-
-        for(int i = 0; i < number.length(); i++){
-            if (i%2 == 0){
-                ergebnis+= number.charAt(i);
-            }
-
-            else {
-                switch (number.charAt(i)){
-                    case '1':
-                        newChar = 'a';
-                        break;
-                    case '2':
-                        newChar = 'b';
-                        break;
-                    case '3':
-                        newChar = 'c';
-                        break;
-                    case '4':
-                        newChar = 'd';
-                        break;
-                    case '5':
-                        newChar = 'e';
-                        break;
-                    case '6':
-                        newChar = 'f';
-                        break;
-                    case '7':
-                        newChar = 'g';
-                        break;
-                    case '8':
-                        newChar = 'h';
-                        break;
-                    case '9':
-                        newChar = 'i';
-                        break;
-                    case '0':
-                        newChar = 'j';
-
-
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + i);
-                }
-                ergebnis+= newChar;
-
-            }
-        }
-
-        return ergebnis;
-    }
 }
--- app/src/main/java/com/example/einzelabgabge_se2/TCP.java
+++ app/src/main/java/com/example/einzelabgabge_se2/TCP.java
@@ -1,15 +1,22 @@
 package com.example.einzelabgabge_se2;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 import java.io.*;
 import java.net.*;
 
 
-public class TCP implements Runnable {
+public class TCP implements Runnable{
 
 
     String martriculationNumber;
     String modiefiedNumber;
+
     BufferedReader inFromServer;
     DataOutputStream outToServer;
     Socket socket = null;
@@ -18,7 +25,6 @@ public class TCP implements Runnable {
     public TCP(String martriculationNumber){
         this.martriculationNumber = martriculationNumber;
     }
-
 //Client-Server Connection
 // Client has to create a Socket
 // IP Adress and Port must be specified
@@ -27,6 +33,7 @@ public class TCP implements Runnable {
     @Override
     public void run() {
 
+        
         try {
             //create client socket to connect to server
             socket = new Socket("se2-isys.aau.at", 53212);
@@ -43,13 +50,15 @@ public class TCP implements Runnable {
             //read the line from server
             modiefiedNumber = inFromServer.readLine();
 
+            setModiefiedNumber(modiefiedNumber);
+
             outToServer.flush();
             inFromServer.close();
             outToServer.close();
             socket.close();
 
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -58,6 +67,12 @@ public class TCP implements Runnable {
     }
     public String getMartriculationNumber() {
         return martriculationNumber;
+    }
+    public void setModiefiedNumber(String modiefiedNumber){
+        this.modiefiedNumber = modiefiedNumber;
+    }
+    public String getModiefiedNumber(){
+        return modiefiedNumber;
     }
 
 }
